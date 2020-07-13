c3162b6d68cc9f07b1985851cd9cdea3be9dbd10
HadarRose <rosenberg07@gmail.com>
2020-07-13 14:25:28 -0500

    Basic post functionality

--- src/PostTweet.java
+++ src/PostTweet.java
@@ -3,9 +3,17 @@ import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class PostTweet {
-    public static void PostTweet() throws TwitterException {
+    public static void main(String[] args) throws TwitterException {
+        if(args.length == 0) { // check for error: no argument
+            throw new IllegalArgumentException("PostTweet.java must have an argument");
+        }
+        postTweet(args[0]);
+
+    }
+
+    public static void postTweet(String post) throws TwitterException {
         Twitter twitter = TwitterFactory.getSingleton();
-        StatusUpdate statusUpdate = new StatusUpdate("Hello World!");
+        StatusUpdate statusUpdate = new StatusUpdate(post);
         Status status = twitter.updateStatus(statusUpdate);
     }
 }
