d50cd60f60267db3de26b2d866a9cfb0204adcd1
HadarRose <rosenberg07@gmail.com>
2020-07-14 15:32:15 -0500

    Respose to feedback
    excluding compiling to JAR w/o maven/IDE tools

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # C102
  ## Bootcamp Lab C102: Simple Java Project- Tweeting
-### Running Instructions
+ ### Dependencies
+ * JDK 8
+ * twitter4j 
+ 
+### Instructions
+* Compiling: 
 * Posting a tweet:
 ```java -jar C102.jar post "post content"``` <br> "post content" must adhere to Twitter's standards (i.e. not an empty 
 string, not above 280 characters)
 * Retrieving timeline:
 ```java -jar C102.jar timeline```
+* Retrieving timeline as a different user: 
+```java -jar C102.jar timeline <key> <key secret> <token> <token secret>```
--- src/GetTimeline.java
+++ src/GetTimeline.java
@@ -2,10 +2,31 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.util.List;
 
 public class GetTimeline {
+
+    public static void getForeignTimeline(String key, String keySecret, String token, String tokenSecret) throws TwitterException {
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setOAuthConsumerKey(key);
+        builder.setOAuthConsumerSecret(keySecret);
+        builder.setOAuthAccessToken(token);
+        builder.setOAuthAccessTokenSecret(tokenSecret);
+        Configuration configuration = builder.build();
+
+        TwitterFactory factory = new TwitterFactory(configuration);
+        Twitter twitter = factory.getInstance();
+
+        List<Status> statusList = twitter.getHomeTimeline();
+        System.out.println("Timeline retrieved");
+        for(Status status: statusList){
+            System.out.println(status.getUser().getName()+":"+status.getText());
+        }
+    }
+
     public static void getTimeline() throws TwitterException {
         Twitter twitter = TwitterFactory.getSingleton();
         List<Status> statusList = twitter.getHomeTimeline();
