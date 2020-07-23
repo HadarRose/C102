cd4abf1da997a7a6e2cca51f945aa259facf9f65
HadarRose <rosenberg07@gmail.com>
2020-07-23 15:55:14 -0500

    Logging and documentation added

--- src/test/java/configuration/ConfigurationTest.java
+++ src/test/java/configuration/ConfigurationTest.java
@@ -6,7 +6,6 @@ import twitter4j.Twitter;
 import static org.junit.Assert.*;
 
 public class ConfigurationTest {
-    // TODO: test loadkeys error
     @Test(expected = Exception.class)
     public void loadKeysTest(){
         Configuration c = new Configuration("filethatdoesntexist");
