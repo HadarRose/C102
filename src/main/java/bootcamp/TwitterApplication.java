6ee8f5f7d9443685cc444e3c682dd6beace83836
HadarRose <rosenberg07@gmail.com>
2020-08-14 14:43:41 -0500

    added CORS support

--- src/main/java/bootcamp/TwitterApplication.java
+++ src/main/java/bootcamp/TwitterApplication.java
@@ -4,6 +4,11 @@ import bootcamp.configuration.ApplicationConfiguration;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TwitterApplication extends Application<ApplicationConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -15,6 +20,11 @@ public class TwitterApplication extends Application<ApplicationConfiguration> {
 
     @Override
     public void run(ApplicationConfiguration config, Environment environment) {
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "http://localhost:9000"); // sets localhost:9000 as authorized requester
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*"); // url pattern to be added after any dispatcher type
+
         TwitterResourceComponent comp = DaggerTwitterResourceComponent.builder()
                 .configurationModule(new ConfigurationModule(config))
                 .build();
